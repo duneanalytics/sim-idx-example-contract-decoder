43c81e44a4cb3b0c100b9b576f8d7f1941757ca5
Brage <5640782+2xic@users.noreply.github.com>
2025-07-22 17:46:13 +0200

    Fix: fix contract name for test

--- listeners/test/Main.t.sol
+++ listeners/test/Main.t.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
-import {Listener} from "../src/Main.sol";
+import {UniswapV3FactoryDecoder} from "src/Main.sol";
 import "sim-idx-sol/Simidx.sol";
 import "sim-idx-generated/Generated.sol";
 import {MockContexts} from "sim-idx-sol/test/MockContexts.sol";
 
 contract ListenerTest is Test {
-    Listener public listener;
+    UniswapV3FactoryDecoder public listener;
 
     function setUp() public {
-        listener = new Listener();
+        listener = new UniswapV3FactoryDecoder();
         vm.recordLogs();
     }
 }
